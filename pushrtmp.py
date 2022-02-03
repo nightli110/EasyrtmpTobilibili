54e87f53ba95fe3bbb8b3c400d9bc2b06913d345
nightli <1658484908@qq.com>
2022-02-03 20:59:06 +0800

    添加按配置选择播放视频集数

--- pushrtmp.py
+++ pushrtmp.py
@@ -9,6 +9,14 @@ config.read("conf", encoding="utf-8")
 
 videotxtpath=config.get("videodir", "video_list")
 videodir = config.get("videodir", "dir")
+playcontinue = config.get("rtmp", "continue")
+
+continueindex=""
+if(playcontinue=='true'):
+    continueconfig = config.get("rtmp", "continuefile")
+    if(continueconfig=="conf"):
+        continueindex = config.get("rtmp", "continueFileIndex")
+
 if os.path.exists(videotxtpath):
     live_address = config.get("rtmp", "live_address")
     live_code = config.get("rtmp", "live_code")
@@ -19,12 +27,15 @@ if os.path.exists(videotxtpath):
     with open(videotxtpath, "r") as videotxt:
         reader = csv.reader(videotxt)
         for line in reader:
-            if len(line)==0:
+            if len(line) == 0:
                 continue
-            videolist[int(line[1])] = videodir+'/'+line[0]
+            videolist[int(line[1])] = videodir + '/' + line[0]
     videolistsize = len(videolist)
+    begin = True
     while(1):
         for i in range(1, videolistsize+1):
+            if(begin):
+                i = int(continueindex)
             ff = FFmpeg(
                 inputs={videolist[i]:'-re '},
                 outputs={rtmp_url:'-vcodec copy -acodec aac -b:a 192k -f flv'}
